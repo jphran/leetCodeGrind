184dd67217980a54eefdc585f8b1bab9bbfa4e64
Justin Francis <j.francis@sarcos.com>
2021-03-01 21:38:56 -0700

    terrible lru cache impl. need to get it to at least O(log n)

--- include/LRUCache.h
+++ include/LRUCache.h
@@ -28,13 +28,19 @@ public:
     }
 
     void put(int key, int value) {
-        mUMap.insert_or_assign(key, value);  // O(log n)
-        mKeyList.remove(key);  // O(n)
-        mKeyList.push_front(key);
-        if(mUMap.size() > mCapacity) {
+        if(get(key) != -1) {
+            mUMap.insert_or_assign(key, value);  // O(log n)
+            return;
+        }
+        else {
+            mUMap.insert(key, value);
+            mKeyList.push_front(key);
+        }
+        if (mUMap.size() > mCapacity) {
             mUMap.erase(mKeyList.back());  // O(log n)
             mKeyList.pop_back();
         }
+
     }
 private:
     int mCapacity;
@@ -50,3 +56,48 @@ private:
  */
 
 #endif //LEETCODEGRIND_LRUCACHE_H
+
+
+
+
+// ********* COMMUNITY SOLN, O(1) list + umap implementation ************
+//class LRUCache {
+//public:
+//
+//    int size;
+//    list<int> lru;                              // MRU ... LRU
+//    unordered_map<int, list<int>::iterator> mp; // key -> iterator
+//    unordered_map<int, int> kv;
+//
+//    LRUCache(int capacity) {
+//        size=capacity;
+//    }
+//
+//    int get(int key) {
+//        if (kv.count(key) == 0) return -1;
+//        updateLRU(key);
+//        return kv[key];
+//    }
+//
+//    void put(int key, int value) {
+//        if (kv.size() == size && kv.count(key) == 0)
+//            evict();
+//        updateLRU(key);
+//        kv[key] = value;
+//    }
+//
+//    void updateLRU(int key) {
+//        if (kv.count(key))
+//            lru.erase(mp[key]);
+//        lru.push_front(key);
+//        mp[key] = lru.begin();
+//    }
+//
+//    void evict() {
+//        mp.erase(lru.back());
+//        kv.erase(lru.back());
+//        lru.pop_back();
+//    }
+//
+//
+//};
