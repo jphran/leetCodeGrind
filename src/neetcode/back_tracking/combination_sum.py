2917fd1b7c76718e04212cf0f8678a574bc1c507
jphran <the.rawc@gmail.com>
2022-06-01 22:29:30 -0700

    finished combination sum, still need to review and study back tracking. doesn't quite make sense to me yet.

--- src/neetcode/back_tracking/combination_sum.py
+++ src/neetcode/back_tracking/combination_sum.py
@@ -15,30 +15,59 @@ All elements of candidates are distinct.
 
 
 class Solution:
-    def _back_track(
-        self,
-        candidates: list[int],
-        target: int,
-        curr_idx: int,
-        curr_combination: list[int],
-        result: list[list[int]],
-    ) -> None:
-        if target == 0:
-            result.append(curr_combination.copy())
-            return
-        if target < 0:
-            return
-
-        for i in range(curr_idx, len(candidates)):
-            curr_combination.append(candidates[i])
-            self._back_track(
-                candidates, target - candidates[i], i, curr_combination, result
-            )
-            curr_combination.pop()
-
     def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
-        results = []
+        # init local vars
+        curr_combination: list[int] = []
+        combination_sums: list[list[int]] = []
 
-        self._back_track(candidates, target, 0, [], results)
+        # sort candidates so, we can cut down the number of decision branches
+        candidates.sort()
 
-        return results
+        def back_track(idx: int, remaining: int) -> None:
+            """Back track any unique repeating combination that reaches target"""
+            # soln found
+            if remaining == 0:
+                combination_sums.append(curr_combination[:])
+                return
+            # non-reachable path
+            if remaining < 0:
+                return
+
+            # try branching
+            for i in range(idx, len(candidates)):
+                curr_combination.append(candidates[i])
+                back_track(i, remaining - candidates[i])
+                curr_combination.pop()
+
+        back_track(0, target)
+
+        return combination_sums
+
+    # WORKING SOLN 5/21/22
+    # def _back_track(
+    #     self,
+    #     candidates: list[int],
+    #     target: int,
+    #     curr_idx: int,
+    #     curr_combination: list[int],
+    #     result: list[list[int]],
+    # ) -> None:
+    #     if target == 0:
+    #         result.append(curr_combination.copy())
+    #         return
+    #     if target < 0:
+    #         return
+    #
+    #     for i in range(curr_idx, len(candidates)):
+    #         curr_combination.append(candidates[i])
+    #         self._back_track(
+    #             candidates, target - candidates[i], i, curr_combination, result
+    #         )
+    #         curr_combination.pop()
+    #
+    # def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+    #     results = []
+    #
+    #     self._back_track(candidates, target, 0, [], results)
+    #
+    #     return results
